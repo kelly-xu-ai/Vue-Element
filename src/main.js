f5f74d5bb18f260a0f02e53626dfbb1fd7aefc26
xulei <18039671980@163.com>
2019-11-26 11:36:15 +0800

    增加路由权限过滤、增加页面加载动画

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './plugins/element.js'
 import './axios'
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 import '@/styles/index.scss' // global css
+import '@/permission' // permission control
 
 Vue.config.productionTip = false
 
